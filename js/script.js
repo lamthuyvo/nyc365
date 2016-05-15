5c09bda3baded1263b7eb0a0e0986a1a0242ae39
lamthuyvo <lamthuyvo@gmail.com>
2016-05-15 02:13:19 -0400

    added audio links and force layout

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 	var windowWidth = $(window).innerWidth();
 		windowHeight = $(window).innerHeight();
 
-	$('.intro-icon').addClass("animated bounce");
+	$('.fa-music').addClass("animated bounce");
 	$('#start h1').addClass("animated bounce");
 
 	// make sections a height of window
@@ -22,21 +22,17 @@ $( document ).ready(function() {
 	var padding = 50;
 	var margin=  {top: 30, right: 0, bottom: 30, left: 0};
 
-
-
 	var happyColor = '#f563f1',
 		mixedColor = '#dbbc3d',
 		unhappyColor = '#009abe';
 
 	//Create scale functions
 	var xScale = d3.scale.linear()
-						// .domain([0, d3.max(ambionicData, function(d, i) { return ambionicData[ambionicData.length-1].days_elapsed; })])
 						.domain([0,365])
 						.range([padding, w-25]);
 	
 
 	var yScale = d3.scale.linear()
-						 // .domain([0, d3.max(ambionicData, function(d) { return ambionicData[ambionicData.length-1].time_fraction })])
 						 .domain([0, 24])
 						 .range([h - padding, padding]);
 
@@ -86,8 +82,11 @@ $( document ).ready(function() {
 					height: h
 				});
 
+	var nodes = ambionicData;
 
-
+	var force = d3.layout.force()
+		    .nodes(nodes)
+		    .size([w, h]);
 
 	
 	//Y-axis label
@@ -97,22 +96,37 @@ $( document ).ready(function() {
 
 	// Create circles
 	function makeGraphic (){
+		//for force layout
+		force.on("tick", tick)
+		    .start();
 
+		var node = svg.selectAll(".node")
+		    .data(nodes)
+		  .enter().append("circle")
+		    .attr("class", "node")
+		    .attr("cx", function(d) { return d.x; })
+		    .attr("cy", function(d) { return d.y; })
+		    .attr("r", 5)
+		    .attr("opacity", 0.5)
+		    .attr("fill", function(d){
+		          if (d.emotion === "negative"){
+		            return unhappyColor
+		          } else if (d.emotion === "positive"){
+		            return happyColor
+		          } else{
+		            return mixedColor
+		          }
+		       })
+		       .attr("class", function(d){ 
+		        if (d.days_elapsed > 213){
+		          return d.emotion + " " + d.storyCategory + " breakup";
+		        } else{
+		          return d.emotion + " " + d.storyCategory;
+		        }
+		        
 
-
-		// circles per row
-
-		svg.selectAll("circle")
-		   .data(ambionicData)
-		   .enter()
-		   .append("circle")
-		   .attr("cx", function(d,i) {
-		   		return xScale(0+i);
-		   })
-		   .attr("cy", function(d,i) {
-		   		return 150;
-		   })
-		   .on("mouseover", function(d) {
+		       })
+		     .on("mouseover", function(d) {
 				if (d.emotion === "negative"){
 		   			var color = unhappyColor
 		   		} else if (d.emotion === "positive"){
@@ -151,36 +165,18 @@ $( document ).ready(function() {
 			        .remove();
 
 			})
-		   .attr("stroke", "#fff")
-		   .attr("fill", function(d){
-		   		if (d.emotion === "negative"){
-		   			return unhappyColor
-		   		} else if (d.emotion === "positive"){
-		   			return happyColor
-		   		} else{
-		   			return mixedColor
-		   		}
-		   })
-		   .attr("class", function(d){ 
-		   	if (d.days_elapsed > 213){
-		   		return d.emotion + " " + d.storyCategory + " breakup";
-		   	} else{
-		   		return d.emotion + " " + d.storyCategory;
-		   	}
-		   	
+		
+		    .on("mousedown", function() { d3.event.stopPropagation(); });
 
-		   })
-		   .attr("r", 0)
-			   	.transition()
-			   	.duration(5)
-			   	.delay(function(d, i) { return i * 5; })
 
-			.attr("r", 7)
-			   	.transition()
-			   	.duration(5)
-			   	.delay(function(d, i) { return i * 5 + 10; })
-		   .attr("r", 5)
-		   .attr("opacity", 0.5);
+		function tick(e) {
+
+
+		  node.attr("cx", function(d) { return d.x; })
+		      .attr("cy", function(d) { return d.y; });
+		}
+
+
 
 		
 	}
@@ -221,10 +217,11 @@ $( document ).ready(function() {
 	})
 
 
-	// section 3: highlight songs that are about city 
+	// section 3: show songs on timeline
 
 	$('#chart-chapter3').waypoint(function(direction) {
 		  	if (direction === 'down') {
+		  		force.stop();
 		  		//Create X axis
 				svg.append("g")
 					.attr("class", "x axis")
@@ -239,8 +236,8 @@ $( document ).ready(function() {
 
 		  		d3.selectAll('circle')
 		  			.transition()
-				   	.duration(5)
-				   	.delay(function(d, i) { return i * 5; })
+				   	.duration(1000)
+				   	.delay(function(d, i) { return i * 2; })
 		  			.attr("cx", function(d) {
 			   		return xScale(d.days_elapsed);
 				   })
@@ -252,6 +249,15 @@ $( document ).ready(function() {
 			    $('#chart-chapter3').animate({"opacity": 1})
 	
 		  	} else if (direction === 'up'){
+
+		  		force.start();
+
+
+		  		d3.selectAll('.axis')
+		  			.transition()
+		  			.duration(500)
+		  			.attr("opacity", 0);
+
 		  		$('#chart-chapter3').animate({"opacity": 0})
 		  		
 		  	}
@@ -265,16 +271,53 @@ $( document ).ready(function() {
 
 	$('#chart-chapter4').waypoint(function(direction) {
 		  	if (direction === 'down') {
+
 		  		// change scale
 		  		xScale.domain([241,365]);
 
-		  		
+		  		// disappear old axis
+		  		d3.selectAll(".x")
+		  			.transition()
+		  			.duration(500)
+		  			.attr("opacity", 0);
+
+		  		var xAxis2 = d3.svg.axis()
+			                  .scale(xScale)
+			                  .orient("bottom")
+			                  .ticks(2)
+			                  .tickValues([241, 365])
+			                  .tickFormat(function(d,i){
+								  	if (d === 241){
+								  		return "the Breakup";
+								  						  		
+								  	} else if (d === 365)
+								  		return "1st year"
+							  });
+
+			    //Create X axis
+				svg.append("g")
+					.attr("class", "x2 axis")
+					.attr("transform", "translate(0," + (h - padding) + ")") // makes the x-axis go on the bottom
+					.call(xAxis2);
 
 		  		// change dot location by 
 		  		d3.selectAll('circle.nyc')
 		  			.transition()
-				   	.duration(5)
-				   	.delay(function(d, i) { return i * 5 + 10; })
+				   	.duration(300)
+		  			.attr("opacity", 0.5)
+		  			.attr("r", 0);
+
+		  		// change dot location by 
+		  		d3.selectAll('circle.work')
+		  			.transition()
+				   	.duration(300)
+		  			.attr("opacity", 0.5)
+		  			.attr("r", 0);
+
+		  		// change dot location by 
+		  		d3.selectAll('circle.none')
+		  			.transition()
+				   	.duration(300)
 		  			.attr("opacity", 0.5)
 		  			.attr("r", 0);
 		  		
@@ -289,15 +332,7 @@ $( document ).ready(function() {
 		  			.attr("opacity", 0.5)
 		  			.attr("r", 5);
 
-		  		// disappear old axis
-		  		xAxis
-                  .tickValues([241, 366])
-                  .tickFormat(function(d,i){
-					  	if (d == 241){
-					  		return "the breakup";
-					  	} else if (d === 366)
-					  		return "1st year"
-					  });
+		  		
 
 		  		
 
@@ -319,6 +354,26 @@ $( document ).ready(function() {
 				   	})
 		  			.attr("r", 5);
 
+		  			d3.selectAll('circle.none')
+		  			.transition()
+		  			.duration(500)
+		  			.delay(function(d, i) { return i * 10; })
+		  			.attr("opacity", 0.5)
+		  			.attr("cy", function(d) {
+				   		return yScale(d.time_fraction);
+				   	})
+		  			.attr("r", 5);
+
+		  			d3.selectAll('circle.work')
+		  			.transition()
+		  			.duration(500)
+		  			.delay(function(d, i) { return i * 10; })
+		  			.attr("opacity", 0.5)
+		  			.attr("cy", function(d) {
+				   		return yScale(d.time_fraction);
+				   	})
+		  			.attr("r", 5);
+
 		  			d3.selectAll('circle.relationship')
 		  			.transition()
 		  			.duration(2000)
@@ -329,18 +384,16 @@ $( document ).ready(function() {
 		  			.attr("opacity", 0.5)
 		  			.attr("r", 5);
 
-		  			// disappear old axis
-			  		xAxis
-	                  .tickValues([0, 183, 366])
-	                  .tickFormat(function(d,i){
-						  	if (d == 0){
-						  		return "arrival in NYC";
-						  	} else if (d === 183){
-						  		// return d + " days";	
-						  		return "first six months";						  		
-						  	} else if (d === 366)
-						  		return "1st year"
-						  });
+		  			d3.selectAll('.x')
+		  				.transition()
+		  				.duration(500)
+		  				.delay(500)
+		  				.attr("opacity",1);
+
+		  			d3.selectAll('.x2')
+		  				.transition()
+		  				.duration(500)
+		  				.attr("opacity",0);
 
 
 
@@ -353,14 +406,27 @@ $( document ).ready(function() {
 
 	$('#chart-chapter5').waypoint(function(direction) {
 		  	if (direction === 'down') {
+		  		$('.fa-headphones').addClass("animated bounce");
 		  		// turn of the lights
 		  		$('body').animate({"background-color": "#000"}, 1000)
 		  		
 			    $('#chart-chapter5').animate({"opacity": 1})
+
+			    // make axes disappear
+			    d3.selectAll('.axis')
+			    	.transition()
+			    	.duration(500)
+			    	.attr("opacity", 0)
 	
 		  	} else if (direction === 'up'){
 		  		$('#chart-chapter5').animate({"opacity": 0})
 		  		$('body').animate({"background-color": "#FFF"}, 500)
+
+		  		//turn axes back on
+		  		d3.selectAll('.axis')
+			    	.transition()
+			    	.duration(500)
+			    	.attr("opacity", 1)
 		  			
 		  	}
 		}, {
@@ -368,6 +434,8 @@ $( document ).ready(function() {
 	})
 
 	// make soundcite
+	// var audio = document.getElementById("audio");
+    // audio.play();
 	
 	function danceParty(){
 		// turn background dark
